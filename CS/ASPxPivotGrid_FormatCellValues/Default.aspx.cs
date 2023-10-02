fbc8a3163b8b5fe684a26f7495a2a81e3f1749ea
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-10-02 17:02:55 +0400

    Change example status to out of maintenance

--- CS/ASPxPivotGrid_FormatCellValues/Default.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace FormatCellValues {
-    public partial class _Default : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
-        }
-    }
-}
